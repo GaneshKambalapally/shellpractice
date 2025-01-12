526a94edf08b29bd19e62fc197cb479124c4d516
RadhikaKadari <radhikaarada4@gmail.com>
2025-01-12 21:43:30 +0530

    git

--- backend.sh
+++ backend.sh
@@ -45,5 +45,5 @@ else
       echo "already installed nodejs"
  fi
 
-dnf install nodejs:20 -y
+dnf install nodejs -y
 VALIDATION $? "failed installation of nodejs:20" "installed nodejs:20 successfully"
