a7874a6ae4aa0443225ee94dfcec49a48920c2cd
RadhikaKadari <radhikaarada4@gmail.com>
2025-01-12 09:54:45 +0530

    git

--- variable-read.sh
+++ variable-read.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
+#echo "Enter User name::"
+#read USERNAME
+#echo "Enter Password::"
+#read PASSWORD
+
+#to hide username and password on screen
 echo "Enter User name::"
-read USERNAME
+read -s USERNAME
+echo "entered username is $USERNAME"
 echo "Enter Password::"
-read PASSWORD
+read -s paSSWORD
+echo "entered password is $paSSWORD
+
