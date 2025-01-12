79827ea8eaabd0605ef67c86f2fb97e7d6df1b43
RadhikaKadari <radhikaarada4@gmail.com>
2025-01-12 09:44:56 +0530

    git

--- variables.sh
+++ variables.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 #examples of variable
-PERSON1=ganesh
-PERSON2=radhika
+#PERSON1=ganesh
+#PERSON2=radhika
+
+#echo "$PERSON1::Hi $PERSON2, how are you?"
+#echo "$PERSON2::Hello $PERSON1, i am good ,thank you."
+
+#same example can be executed by passing the values in runtime
+
+PERSON1=$1
+PERSON2=$2
 
 echo "$PERSON1::Hi $PERSON2, how are you?"
 echo "$PERSON2::Hello $PERSON1, i am good ,thank you."
