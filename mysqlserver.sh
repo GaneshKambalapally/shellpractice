986acf5f1e080b3839f45aaff3b801632d50cf2f
RadhikaKadari <radhikaarada4@gmail.com>
2025-01-12 16:57:37 +0530

    git

--- mysqlserver.sh
+++ mysqlserver.sh
@@ -15,8 +15,25 @@ CHECK_ROOT(){
     fi 
 }
 
+VALIDATION(){
+
+    if [ $1 -ne 0 ]
+    then
+ echo "Command failure....$3"
+ else
+ echo "Command success....$2"
+ exit 1
+ fi
+
+}
+
 CHECK_ROOT
 
 #installation of mysql
  #check whether mysql already installed
  dnf list installed mysqld
+ VALIDATION $? "mysql not installed" "mysql already installed"
+
+ #installation of mysql-server
+ dnf install mysql-server -y
+ 
