0d1351173ee81b96b74fcf473a8c47dd3334948a
RadhikaKadari <radhikaarada4@gmail.com>
2025-01-12 10:07:48 +0530

    git

--- array.sh
+++ array.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 PERSONS=("ganesh" "radhika" "himansh" "ganvika")
-echo "name of father is $PERSONS[0]"
+echo "name of father is ${PERSONS[0]}"
 echo "name of mother is $PERSONS[1]"
 echo "name of son is $PERSONS[2]"
 echo "name of daugther is $PERSONS[3]"
