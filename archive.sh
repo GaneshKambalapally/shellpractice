1080d097753e0136bbf4f06d25a732ccbaa90dde
RadhikaKadari <radhikaarada4@gmail.com>
2025-01-16 21:50:58 +0530

    messages

--- archive.sh
+++ archive.sh
@@ -1,9 +1,18 @@
 #!/bin/bash/
 
 SOURCE_DIR=/home/ec2-user/sourcedir
+DESTINATION_DIR=/home/ec2-user/destinationdir
+
 if [ -d $SOURCE_DIR ]
 then echo "source is exist"
 else
 echo "source doesnot exist"
 exit 1
 fi
+if [ -d $DESTINATION_DIR ]
+then echo "destination is exist"
+else
+echo "destination doesnot exist"
+exit 1
+fi
+
