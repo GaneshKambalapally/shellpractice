a127ba0ef51e5a8185b2bd75eaa704108931d384
RadhikaKadari <radhikaarada4@gmail.com>
2025-01-17 14:08:11 +0530

    messages

--- concatination.sh
+++ concatination.sh
@@ -4,7 +4,8 @@ echo "enter the input file1"
 read inputfile1
 echo "enter the input file2"
 read inputfile2
+OUTPUT="/home/ec2-user/shellpractice/output.txt"
 
-$inputfile1$inputfile2 >> touch outputfile.txt 
+$inputfile1$inputfile2 &>>outputfile.txt 
 
 # echo "out put file is:" cat $outputfile
