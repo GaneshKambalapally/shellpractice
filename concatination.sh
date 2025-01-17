45f37ef24554142d577c90602bb1a190ab7a264a
RadhikaKadari <radhikaarada4@gmail.com>
2025-01-17 14:02:05 +0530

    messages

--- concatination.sh
+++ concatination.sh
@@ -4,9 +4,7 @@ echo "enter the input file1"
 read inputfile1
 echo "enter the input file2"
 read inputfile2
-echo "enter the output file"
-read outputfile
 
-$(inputfile1 && inputfile2) | $outputfile -@
+$(inputfile1 && inputfile2) | touch outputfile.txt 
 
 # echo "out put file is:" cat $outputfile
